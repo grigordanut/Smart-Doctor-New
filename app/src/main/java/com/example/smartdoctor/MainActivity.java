bacfbac4ff3f169556ececa3edfefe1d35d29610
Grigor Danut <grigor@seamlesscare.ie>
2023-09-26 08:16:40 +0100

    Added more code in the Main activity.

--- app/src/main/java/com/example/smartdoctor/MainActivity.java
+++ app/src/main/java/com/example/smartdoctor/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,13 +16,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn_RegisterMain = findViewById(R.id.btnRegisterMain);
-        btn_RegisterMain.setOnClickListener(new View.OnClickListener() {
+        Button btn_LogInMain = findViewById(R.id.btnLogInMain);
+        btn_LogInMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent_Reg = new Intent(MainActivity.this, CheckUniqueCode.class);
-                startActivity(intent_Reg);
+                startActivity(new Intent(MainActivity.this, LoginBy.class));
             }
         });
+
+        //Action button Register
+        Button btn_registerMain = findViewById(R.id.btnRegisterMain);
+        btn_registerMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sign = new Intent(MainActivity.this, CheckUniqueCode.class);
+                startActivity(sign);
+            }
+        });
+
+        //Action button NFC TEST
+        Button buttonNFC = findViewById(R.id.btnNfc);
+        buttonNFC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent nfc = new Intent(MainActivity.this, NFCActivity.class);
+//                startActivity(nfc);
+            }
+        });
+    }
+
+    //user log out
+    private void HospitalsList(){
+        startActivity(new Intent(MainActivity.this, HospitalsList.class));
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu_restaurant; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (item.getItemId() == R.id.contactUs) {
+            HospitalsList();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
