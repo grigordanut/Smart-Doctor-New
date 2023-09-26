a031e0d9df8b3140838b6a9e8c8ad30080e0de5f
Grigor Danut <grigor@seamlesscare.ie>
2023-09-26 09:02:31 +0100

    Added LoginBy activity.

--- app/src/main/java/com/example/smartdoctor/LoginBy.java
+++ app/src/main/java/com/example/smartdoctor/LoginBy.java
@@ -2,7 +2,10 @@ package com.example.smartdoctor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginBy extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class LoginBy extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_by);
+
+        //log in by using log in details
+        Button btn_EnterDetails = findViewById(R.id.btnEnterDetails);
+        btn_EnterDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginBy.this, Login.class));
+            }
+        });
+
+        //log in by using Finger Print
+        Button btn_FingerPrint = findViewById(R.id.btnFingerPrint);
+        btn_FingerPrint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginBy.this, FingerPrintScan.class));
+            }
+        });
     }
 }
