c1a023bd3d7dd1ef04b32279a94d73045f2f2fa8
Grigor Danut <grigor@seamlesscare.ie>
2023-09-26 06:52:23 +0100

    Added the code in the activity CheckUniqueCode.

---
+++ app/src/main/java/com/example/smartdoctor/HospitalsListAddDoctor.java
@@ -0,0 +1,14 @@
+package com.example.smartdoctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HospitalsListAddDoctor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospitals_list_add_doctor);
+    }
+}
