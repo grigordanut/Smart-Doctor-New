34ca38e0b222078990d4ee9e6c40689ddbd18d5c
Grigor Danut <grigor@seamlesscare.ie>
2023-09-26 08:54:23 +0100

    Added Hospital Registration activity.

--- app/src/main/java/com/example/smartdoctor/MainActivity.java
+++ app/src/main/java/com/example/smartdoctor/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu_restaurant; this adds items to the action bar if it is present.
+        // Inflate the menu_smart_doctor; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
